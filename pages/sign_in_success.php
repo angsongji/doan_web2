7e13af976ce4d9d4c3ced031b326ec72f75ebdca
voanhtuan697 <anhtuanvo697@gmail.com>
2024-04-03 11:06:06 +0700

    Add them phan sign va log

---
+++ pages/sign_in_success.php
@@ -0,0 +1,23 @@
+<div class="wrapper_sucess-signin">
+            <div class="title_sucess-signin">
+                <h2>Đăng ký tài khoản Meme</h2>
+            </div>
+            <div class="paragraph_sucess-signin">
+                <p><b>Chúc mừng bạn đã đăng ký tài khoản thành công!</b><br>
+                    Bây giờ, bạn đã sẵn sàng để khám phá thế giới điện ảnh đầy thú vị tại Meme. <br><br>
+                    <b>Dưới đây là một số lợi ích khi bạn trở thành thành viên của chúng tôi:</b> <br><br>
+                <ul>
+                    <li><b>Đặt vé xem phim nhanh chóng và dễ dàng:</b>Chỉ với vài thao tác đơn giản, bạn có thể đặt vé
+                        cho bất kỳ bộ phim nào bạn muốn, bất cứ lúc nào, bất cứ nơi đâu.</li>
+                    <li><b>Thanh toán an toàn và tiện lợi:</b> Chúng tôi hỗ trợ nhiều phương thức thanh toán an toàn và
+                        tiện lợi để bạn lựa chọn.</li>
+                    <li><b>Cập nhật thông tin phim mới nhất:</b> Bạn sẽ luôn được cập nhật những thông tin mới nhất về
+                        các bộ phim sắp ra mắt, các chương trình khuyến mãi hấp dẫn, và nhiều hơn nữa.</li>
+                    <li><b>Lưu trữ lịch sử đặt vé:</b> Bạn có thể dễ dàng theo dõi lịch sử đặt vé của mình và quản lý vé
+                        xem phim của bạn.</li>
+                </ul>
+                <br>
+                <b>Hãy bắt đầu hành trình điện ảnh của bạn ngay hôm nay!</b>
+                </p>
+            </div>
+        </div>

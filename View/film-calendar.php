817047d0a8b0c8ca177d345dab5ddf98d6dd6ae2
ttt2205 <thanhtrung22052004@gmail.com>
2024-04-05 14:50:38 +0700

    add chi-tiet-film

---
+++ View/film-calendar.php
@@ -0,0 +1,65 @@
+<?php
+    echo '
+        <div class="filmCalendar">
+            <h4>Lịch Chiếu</h4>
+
+            <div class="calendar">
+                <div class="list-days">
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+
+                    <div class="day">
+                        <label>Thứ 5</label>
+                        <p>23/03/2034</p>
+                    </div>
+                </div>
+
+                <div class="list-hours">
+                    <div class="hour">
+                        13h00
+                    </div>
+                    <div class="hour">
+                        13h00
+                    </div>
+                    <div class="hour">
+                        13h00
+                    </div>
+                    <div class="hour">
+                        13h00
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pre and Next -->
+            <div class="pre">
+                <div id="btn-left">&lsaquo;</div>
+            </div>
+
+            <div class="next">
+            <div id="btn-right"> &rsaquo;</div>
+            </div>
+        </div>
+    ';
+?>

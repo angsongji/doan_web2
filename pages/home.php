1eb7d706f8b15d012977ea680c5ecd26443df702
tiendoha <trannhattien.2004tv@gmail.com>
2024-04-12 22:08:22 +0700

    Trang chu

---
+++ pages/home.php
@@ -0,0 +1,177 @@
+<?php 
+echo '
+    <section>
+        <div class="content">
+            <div class="wrapper row">
+                <div class="phimDangChieu">Phim đang chiếu</div>
+                <div class="hinhanh">
+                <div class="hinhanh1 col"><img src="./img/poster-Mai-scaled.jpg">
+                    <h1>1</h1>
+                    <h3>MAI</h3>
+                    <p>Lãng Mạn</p>
+                    <img class="ngoisao" src="./img/ngoisao.png">
+                    <h5>9.1</h5>
+                    <h6>18+</h6>
+                    <i class="fa-solid fa-circle-play" id="start1"></i>
+                </div>
+                <div class="hinhanh2 col"><img src="./img/madame_web.jpg">
+                    <h1>2</h1>
+                    <h3>Madame Web</h3>
+                    <p>Khoa Học Viễn Tưởng, Hành Động</p>
+                    <img class="ngoisao" src="./img/ngoisao.png">
+                    <h5>6.3</h5>
+                    <h6>13+</h6>
+                    <i class="fa-solid fa-circle-play" id="start2"></i>
+                </div>
+                <div class="hinhanh3 col"><img src="./img/gaplaichibau.jpg">
+                    <h1>3</h1>
+                    <h3>Gặp Lại Chị Bầu</h3>
+                    <p>Hài, Lãng Mạn</p>
+                    <img class="ngoisao" src="./img/ngoisao.png">
+                    <h5>8.6</h5>
+                    <h6>13+</h6>
+                    <i class="fa-solid fa-circle-play" id="start3"></i>
+                </div>
+                <div class="hinhanh4 col"><img src="./img/thanhguomdietquy.jpg">
+                    <h1>4</h1>
+                    <h3>Thanh Gươm Diệt Quỷ</h3>
+                    <p>Hoạt Hình, Hành Động</p>
+                    <img class="ngoisao" src="./img/ngoisao.png">
+                    <h5>9.9</h5>
+                    <h6>16+</h6>
+                    <i class="fa-solid fa-circle-play" id="start4"></i>
+                </div>
+                <div class="hinhanh5 col"><img src="./img/nangtho.jpg">
+                    <h1>5</h1>
+                    <h3>Nàng Thơ của Miller</h3>
+                    <p>Chính Kịch, Gây Cấn</p>
+                    <img class="ngoisao" src="./img/ngoisao.png">
+                    <h5>10</h5>
+                    <h6>18+</h6>
+                    <i class="fa-solid fa-circle-play" id="start5"></i>
+                    <i class="fa-solid fa-circle-chevron-right" id="next"></i>
+                </div>
+               
+
+            </div>
+                
+            </div>
+            <div class="phimsapchieu">
+                <div class="hinhanhsapchieu">
+                    <div class="phimSapChieu">Phim sắp chiếu</div>
+                <div class="hinhanh6"><img src="./img/kungfupanda.jpg">
+                    <h3>Kung Fu Panda 4</h3>
+                    <p>Hài, Hoạt Hình</p>
+                    <h6>P</h6>
+                    <i class="fa-solid fa-circle-play" id="start6"></i>
+                </div>
+                <div class="hinhanh7"><img src="./img/nhungmanhghepcamxusc.jpg">
+                    <h3>Những mảnh ghép cảm </h3>
+                    <p>Hài, Hoạt Hình</p>
+                    <h6>P</h6>
+                    <i class="fa-solid fa-circle-play" id="start7"></i>
+                </div>
+                <div class="hinhanh8"><img src="./img/godzila.jpg">
+                    <h3>Godzilla x Kong</h3>
+                    <p>Khoa Học Viễn Tưởng, Phiêu Lưu</p>
+                    <h6>13+</h6>
+                    <i class="fa-solid fa-circle-play" id="start8"></i>
+                </div>
+                <div class="hinhanh9"><img src="./img/mickey17.jpg">
+                    <h3>MicKey 17</h3>
+                    <p>Khoa Học Viễn Tưởng, Phiêu Lưu</p>
+                    <h6>16+</h6>
+                    <i class="fa-solid fa-circle-play" id="start9"></i>
+                </div>
+                <div class="hinhanh10"><img src="./img/sangden.jpg">
+                    <h3>Sáng Đèn</h3>
+                    <p>Hài, Tình Cảm</p>
+                    <h6>K</h6>
+                    <i class="fa-solid fa-circle-play" id="start10"></i>
+                    <i class="fa-solid fa-circle-chevron-right" id="next2"></i>
+                </div>
+                </div>
+            </div>
+            <div class="locphim">
+                <div class="hinhanhtimkiem">
+                    <div class="LocPhim">Tìm phim chiếu rạp trên MEME</div>
+                <select name="theloai" id="theloai">
+                    <option value="1" selected>Tất Cả</option>
+                </select>
+                <select name="quocgia" id="quocgia">
+                    <option value="1" selected>Quốc Gia</option>
+                </select>
+                <select name="nam" id="nam">
+                    <option value="1">Năm</option>
+                </select>
+                <input type="text" placeholder="Tìm Tên Phim" id="nhaptimkiem">
+
+                <div class="hinhanh11"><img src="./img/b4s.jpg">
+                    <h3>B4S - Trước Giờ Yêu</h3>
+                    <p>Hài, Tình Cảm</p>
+                    <h6>18+</h6>
+                    <i class="fa-solid fa-circle-play" id="start11"></i>
+                </div>
+                <div class="hinhanh12"><img src="./img/furiosa.jpg">
+                    <h3>Furiosa</h3>
+                    <p>Khoa Học Viễn Tưởng, Hành Động</p>
+                    <h6>16+</h6>
+                    <i class="fa-solid fa-circle-play" id="start12"></i>
+                </div>
+                <div class="hinhanh13"><img src="./img/garfield.jpg">
+                    <h3>Garfield-Mèo Béo Siêu Quậy</h3>
+                    <p>Hài, Hoạt Hình</p>
+                    <h6>P</h6>
+                    <i class="fa-solid fa-circle-play" id="start13"></i>
+                </div>
+                <div class="hinhanh14"><img src="./img/hanhtinhcat.jpg">
+                    <h3>Hành Tinh Cát: Phần Hai</h3>
+                    <p>Khoa Học Viễn Tưởng, Phiêu Lưu</p>
+                    <h6>13+</h6>
+                    <i class="fa-solid fa-circle-play" id="start14"></i>
+                </div>
+                <div class="hinhanh15"><img src="./img/senboss.jpg">
+                    <h3>Sen Boss Sum Vầy</h3>
+                    <p>Hài, Chính Kịch</p>
+                    <h6>K</h6>
+                    <i class="fa-solid fa-circle-play" id="start15"></i>
+                </div>
+
+                <div class="hinhanh16"><img src="./img/quatmotrungma.jpg">
+                    <h3>Quật Mộ Trùng Ma</h3>
+                    <p>Kinh dị, Bí Ẩn</p>
+                    <h6>18+</h6>
+                    <i class="fa-solid fa-circle-play" id="start16"></i>
+                </div>
+                <div class="hinhanh17"><img src="./img/thanhguomdietquy.jpg">
+                    <h3>Thanh Gươm Diệt Quỷ</h3>
+                    <p>Hoạt Hình, Hành Động</p>
+                    <h6>16+</h6>
+                    <i class="fa-solid fa-circle-play" id="start17"></i>
+                </div>
+                <div class="hinhanh18"><img src="./img/gaplaichibau.jpg">
+                    <h3>Gặp Lại Chị Bầu</h3>
+                    <p>Hài, Lãng Mạn</p>
+                    <h6>13+</h6>
+                    <i class="fa-solid fa-circle-play" id="start18"></i>
+                </div>
+                <div class="hinhanh19"><img src="./img/nangtho.jpg">
+                    <h3>Nàng Thơ Của Miller</h3>
+                    <p>Chính Kịch, Gây Cấn</p>
+                    <h6>18+</h6>
+                    <i class="fa-solid fa-circle-play" id="start19"></i>
+                </div>
+                <div class="hinhanh20"><img src="./img/sangden.jpg">
+                    <h3>Sáng Đèn</h3>
+                    <p>Hài, Tình Cảm</p>
+                    <h6>K</h6>
+                    <i class="fa-solid fa-circle-play" id="start20"></i>
+                </div>
+            </div>
+                </div>
+                
+        </div>
+    </section>
+    <script src="../js/trangchu.js"></script>'
+
+?>

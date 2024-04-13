375fde98a5d4bc7386bee717ba45fc50c20f4955
ttt2205 <thanhtrung22052004@gmail.com>
2024-04-13 07:43:33 +0700

    them chi tiet phim

--- pages/container-popup-menu-chon-nuoc.php
+++ pages/container-popup-menu-chon-nuoc.php
@@ -1,65 +1,33 @@
 <?php
-    echo '
-    <div class="container-pop-up-menu-chon-nuoc">
+    include("../database/connectDatabase.php");
+    $connect = new connectDatabase("localhost", "root", "", "cinema");
+    $dichVuSql = "SELECT * FROM dichvu";
+    $dichVuQuery = $connect->executeQuery($dichVuSql);
+?>
+
+<div class="container-pop-up-menu-chon-nuoc">
     <div class="header-menu-chon-nuoc">
         <h3>Combo - Bắp Nước</h3>
         <div id="icon-close-menu-chon-nuoc">&times;</div>
     </div>
 
     <div id="menu-bap-nuoc">
+        <?php while($dichVuRow = mysqli_fetch_assoc($dichVuQuery)) { ?>
         <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
+            <img src="../img/<?php echo $dichVuRow['NAMEANH']; ?>" alt="">
 
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
+            <div class="thong-tin-dich-vu">
+                <div class="ten-dich-vu"><?php echo $dichVuRow['TENDICHVU']; ?></div>
+                <div class="mo-ta-dich-vu"><?php echo $dichVuRow['MOTA']; ?></div>
+                <div class="gia-dich-vu"><?php echo $dichVuRow['PRICE']; ?></div>
+                <div class="so-luong-dich-vu">
+                    <button name="<?php echo $dichVuRow['MADICHVU'].'-btn-tru'; ?>">-</button>
+                    <input type="text" name="<?php echo $dichVuRow['MADICHVU'].'-input'; ?>" value="0">
+                    <button name="<?php echo $dichVuRow['MADICHVU'].'-btn-cong'; ?>">+</button>
+                </div>
+            </div>
         </div>
-
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
-        <div class="mon">
-            <img src="" alt="">
-            <div></div>
-        </div>
+        <?php } ?>
     </div>
 
     <div id="tong-tien-bap-nuoc">
@@ -71,5 +39,3 @@
         <h4>Tiếp tục</h4>
     </div>
 </div>
-    ';
-?>

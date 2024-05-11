aafe1ed378150284d16009fa3cda0aded8d6541d
ttt2205 <thanhtrung22052004@gmail.com>
2024-05-12 00:57:26 +0700

    chucnang: xu ly uu dai theo tong tien ve

---
+++ pages/load-du-lieu-uu-dai.php
@@ -0,0 +1,30 @@
+<?php
+    require_once('../database/connectDatabase.php');
+    $connect = new connectDatabase();
+    $tongtien = json_decode(file_get_contents('php://input'), true);
+
+    if(isset($tongtien)) {
+        $tongTienThanhToan = intval($tongtien['tongtien']);
+        $uuDaiSql = "SELECT * FROM uudai
+                WHERE $tongTienThanhToan >= DIEUKIEN";
+        $uuDaiQuery = $connect->executeQuery($uuDaiSql);
+
+        $uuDaiArr = array();
+        while($row = mysqli_fetch_assoc($uuDaiQuery)) {
+            $uuDaiArr[$row['CODE']] = array("phantramuudai" => $row['PHANTRAMUUDAI'], "tenuudai" => $row['TENUUDAI'], "dieukien" => $row['DIEUKIEN']);
+        }
+
+        // Kiếm phần trăm ưu đãi cao nhất
+        $maxPhanTramUuDai = 0;
+        $tenUuDai = '';
+        foreach(array_values($uuDaiArr) as $value) {
+            if(is_numeric($value['dieukien']))
+                if($maxPhanTramUuDai <= $value['phantramuudai']) {
+                    $maxPhanTramUuDai = $value['phantramuudai'];
+                    $tenUuDai = $value['tenuudai'];
+                }
+        }
+
+        echo "<div style='font-size: 16px; font-weight: bold' phantramuudai='$maxPhanTramUuDai'>'$tenUuDai'</div>";
+    }
+?>
